068644c5ceabbb6fff65ed66acc4a14ec063d6a8
Zain <uk974962@gmail.com>
2025-12-29 10:35:53 +0500

    server fixes

--- server.js
+++ server.js
@@ -37,4 +37,4 @@ io.on('connection', (socket) => {
 });
 
 
-server.listen(5000, () => console.log('Server running on port 5000'));
+server.listen(5001, () => console.log('Server running on port 5001'));
